108d941ef2f1f47a593f1ddfe4fd7993b61b8233
ElijahDowl <43911395+ElijahDowl@users.noreply.github.com>
2019-03-03 10:30:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 // all jQuery methods go here...
 
+	$(".dance-icon").hover(onHovered, notHovered);
+	function onHovered(){
+		let str = $(this)[0].src;
+		let name = str.substring(0, str.length - 8);
+		$(this)[0].src = name + "giphy.gif";
 
-
-
-
-
-
-
+	}
+	function notHovered (){
+		let str = $(this)[0].src;
+		let name = str.substring(0, str.length - 9);
+		$(this)[0].src = name + "icon.png";
+	}
+//runningman-icon.png --->. runningman-giphy.gif
 
 
 
